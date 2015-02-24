adc72a839997d08364fe4dc27f15d743e6879ab6
sqazi1004 <sqazi1004@users.noreply.github.com>
2015-02-23 17:55:48 -0800

    Update and rename src/models to src/models/Image.java
    Image class to choose board

---
+++ src/models/Image.java
@@ -0,0 +1,71 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Image extends JFrame {
+
+	public Image() {
+
+		String input = JOptionPane
+				.showInputDialog("Please choose your board: Greek, Norse or Egyptian:");
+		if (input.equalsIgnoreCase("Greek")) {
+			initGreek();
+		}
+
+		if (input.equalsIgnoreCase("Norse")) {
+			initNorse();
+		}
+
+		if (input.equalsIgnoreCase("Egyptian")) {
+			initEgypt();
+		}
+	}
+
+	public void initGreek() {
+
+		add(new GreekBoard());
+
+		pack();
+
+		setTitle("Greek Board");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public void initNorse() {
+
+		add(new NorseBoard());
+
+		pack();
+
+		setTitle("Norse Board");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public void initEgypt() {
+
+		add(new EgyptBoard());
+
+		pack();
+
+		setTitle("Egypt Board");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Image board = new Image();
+				board.setVisible(true);
+			}
+		});
+
+	}
+
+}
